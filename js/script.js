0c9944bebc673b0db20a40239f405e3d39921337
distrik <kuanbek012@gmail.com>
2021-01-19 17:00:01 +0500

    answers is done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 let numberOfFilms = prompt("Сколько фильмов вы посмотрели?");
+let numberOfMovies = prompt("Последний фильм который вы смотрели?");
+let numberOfBalls = prompt("На сколько оцените?");
 
 let personalMovieDB = {
     count: numberOfFilms,
-    movies: {},
+    movies: {
+        numberOfMovies,
+        numberOfBalls
+    },
     actors: {},
     genres: [],
     privat: false
 };
+
+console.log(personalMovieDB.movies);
