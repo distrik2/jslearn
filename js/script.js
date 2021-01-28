efd286663f46a8f72e2f82e137270e7af0570340
distrik <kuanbek012@gmail.com>
2021-01-28 22:59:26 +0500

    33 задача, то что в коментах, сделал я, но не правильно. Оказывается, можно через функцию сделать

--- js/script.js
+++ js/script.js
@@ -13,32 +13,78 @@ const movieDB = {
 
 const promo = document.querySelector(".promo"),
     promoAdv = promo.querySelectorAll(".promo__adv img"),
-    // promoAdv = promo.querySelector(".promo__adv"),
     promoGenre = promo.querySelector(".promo__genre"),
     promoBg = promo.querySelector(".promo__bg"),
-    // promoInteractiveItem = promo.querySelectorAll(".promo__interactive-item"),
-    movieList = document.querySelector('.promo__interactive-list');
+    movieList = document.querySelector('.promo__interactive-list'),
+    addingInput = document.querySelector(".adding__input"),
+    button = document.querySelector("button"),
+    deleteElm = document.querySelector(".delete"),
+    chekBoxInput = document.querySelector("input[type=checkbox]");
 
-// promoAdv.remove();
 promoAdv.forEach(item => {
     item.remove();
 });
 
-promoGenre.innerText = "драма";
 
+promoGenre.innerText = "драма";
 promoBg.style.backgroundImage = " url('img/bg.jpg ') ";
 
-movieDB.movies.sort();
+const sortArr = (arr) => {
+    arr.sort();
+};
 
+button.addEventListener("click", (event) => {
+    event.preventDefault();
 
-movieList.innerHTML = "";
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-    <li class="promo__interactive-item">${i + 1} ${film}
-    <div class="delete"></div>
-    </li>
-    `;
+    if (addingInput.value != "" && addingInput.value != " ") {
+        if (addingInput.value.length > 21) {
+            addingInput.value = addingInput.value.substr(0, 21) + "...";
+        }
+
+        if (chekBoxInput) {
+            console.log("Добавляем любимый фильм - ", addingInput.value);
+        }
+        
+        movieDB.movies.push(addingInput.value);
+        createMovieList(movieDB.movies, movieList);
+        // movieList.innerHTML += `
+        // <li class="promo__interactive-item">${movieDB.movies.length}. ${addingInput.value}
+        // <div class="delete"></div>
+        // </li>
+        // `;
+        addingInput.value = "";
+    }
 });
-// for (let i = 0; i < promoInteractiveItem.length; i++){
-//     promoInteractiveItem[i].innerHTML = `${i + 1} ` + movieDB.movies[i];
-// }
+
+// movieList.innerHTML = "";
+// sortArr(movieDB.movies);
+// movieDB.movies.forEach((film, i) => {
+//     movieList.innerHTML += `
+//     <li class="promo__interactive-item">${i + 1}. ${film}
+//     <div class="delete"></div>
+//     </li>
+//     `;
+// });
+
+function createMovieList(films, parent) {
+    parent.innerHTML = "";
+    sortArr(films);
+
+    films.forEach((film, i) => {
+        parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1} ${film}
+                    <div class="delete"></div>
+                </li>
+            `;
+    });
+
+    document.querySelectorAll('.delete').forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            movieDB.movies.splice(i, 1);
+
+            createMovieList(films, parent);
+        });
+    });
+}
+createMovieList(movieDB.movies, movieList);
