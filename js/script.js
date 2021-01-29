234b23d76350b479b6a0b0e0e48013f079a11e93
distrik <kuanbek012@gmail.com>
2021-01-29 15:54:57 +0500

    33 урок

--- js/script.js
+++ js/script.js
@@ -19,13 +19,11 @@ document.addEventListener("DOMContentLoaded", () => {
         movieList = document.querySelector('.promo__interactive-list'),
         addingInput = document.querySelector(".adding__input"),
         button = document.querySelector("button"),
-        deleteElm = document.querySelector(".delete"),
         chekBoxInput = document.querySelector("input[type=checkbox]");
 
     promoAdv.forEach(item => {
         item.remove();
     });
-
 
     promoGenre.innerText = "драма";
     promoBg.style.backgroundImage = " url('img/bg.jpg ') ";
@@ -39,33 +37,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (addingInput.value != "" && addingInput.value != " ") {
             if (addingInput.value.length > 21) {
-                addingInput.value = addingInput.value.substr(0, 21) + "...";
+                addingInput.value = addingInput.value.substr(0, 20) + "...";
             }
-
-            if (chekBoxInput) {
+            if (chekBoxInput.checked) {
                 console.log("Добавляем любимый фильм - ", addingInput.value);
             }
-
             movieDB.movies.push(addingInput.value);
             createMovieList(movieDB.movies, movieList);
-            // movieList.innerHTML += `
-            // <li class="promo__interactive-item">${movieDB.movies.length}. ${addingInput.value}
-            // <div class="delete"></div>
-            // </li>
-            // `;
             addingInput.value = "";
         }
     });
-
-    // movieList.innerHTML = "";
-    // sortArr(movieDB.movies);
-    // movieDB.movies.forEach((film, i) => {
-    //     movieList.innerHTML += `
-    //     <li class="promo__interactive-item">${i + 1}. ${film}
-    //     <div class="delete"></div>
-    //     </li>
-    //     `;
-    // });
 
     function createMovieList(films, parent) {
         parent.innerHTML = "";
