b6494ffd1031fb67f370401072599409f8b2d824
distrik <kuanbek012@gmail.com>
2021-02-04 15:18:26 +0500

    добавлен код таймера

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     
+    const tabsparent = document.querySelector(".tabheader__items");
     const tabs = document.querySelectorAll(".tabheader__item");
     const tabscontent = document.querySelectorAll(".tabcontent");
-    const tabsparent = document.querySelector(".tabheader__items");
 
     function hideTabContent() {
         tabscontent.forEach(item => {
@@ -40,4 +40,58 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+
+    // timet
+    const deadline = "2021-02-05";
+
+    function getTimeRem(endtime){
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        const days = Math.floor(t / (1000 * 60 * 60 * 24) );
+        const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((t / 1000 / 60) % 60);
+        const seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            t,
+            days,
+            hours,
+            minutes,
+            seconds,
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10){
+            return `0${num}`;
+        }
+        else{
+            return num;
+        }
+    }
+
+    function setClock(endtime){
+        const timer = document.querySelector(".timer");
+        const days = timer.querySelector("#days");
+        const hours = timer.querySelector("#hours");
+        const minutes = timer.querySelector("#minutes");
+        const seconds = timer.querySelector("#seconds");
+        const timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock(){
+            const t = getTimeRem(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.t <= 0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock(deadline);
 });
